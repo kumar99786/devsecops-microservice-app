25a7651ce88eea80887f723d4a5f54bcbd1a0d18
kumar99786 <147326244+kumar99786@users.noreply.github.com>
2026-02-10 01:05:50 +0530

    Add Flask API with home and health routes

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,19 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/api")
+def home():
+    return jsonify({
+        "message": "Hello from Backend API",
+        "status": "success"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "UP"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
